bddc0861a84b61c9030716406f9190ca9335ec6f
tpettrov <apetrov@vkp.prb>
2017-10-13 11:19:41 +0300

    backCounter the smart way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PriceComponent from './components/PriceComponent';
@@ -8,46 +8,59 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {price: 0, updateInterval: 0};
+        this.state = {price: 0, backCounterValue: 7};
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.getStats();
-        this.setState({updateInterval: setInterval(this.getStats.bind(this), 7000)});
+        this.setState({updateInterval: setInterval(this.count.bind(this), 1000)});
     }
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.state.updateInterval);
     }
 
-    //https://stackoverflow.com/questions/19135143/synchronization-of-two-timers-setinterval
+
+    count() {
+
+        if (this.state.backCounterValue == 0) {
+            this.getStats();
+            this.setState(previousState => {
+            return {backCounterValue: previousState.backCounterValue + 7}
+            });
+        } else {
+
+            this.setState(previousState => {
+                return {backCounterValue: previousState.backCounterValue - 1};
+            });
+        }
+    }
 
     getStats() {
 
         let that = this;
         let url = 'https://api.coinmarketcap.com/v1/ticker/ethereum/';
-        fetch(url).then(function(response) {
+        fetch(url).then(function (response) {
             return response.json()
-        }).then(function(data) {
+        }).then(function (data) {
             that.setState({price: data[0].price_usd});
         })
 
     }
 
 
-
     render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to CryptoReact</h2>
-        </div>
-          <BackCounterComponent/>
-        <PriceComponent price={this.state.price}></PriceComponent>
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to CryptoReact</h2>
+                </div>
+                <BackCounterComponent backCounter={this.state.backCounterValue}/>
+                <PriceComponent price={this.state.price}></PriceComponent>
+            </div>
+        );
+    }
 }
 
 export default App;

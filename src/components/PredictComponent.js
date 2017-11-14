c4bdd58d4e0e1f601b26b98f8907fcc549477dc3
tpettrov <tpettrov@gmail.com>
2017-11-14 17:06:25 +0200

    attempts added

--- src/components/PredictComponent.js
+++ src/components/PredictComponent.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import mainStore from '../stores/MainStore';
 
 class PredictComponent extends Component {
 
@@ -6,17 +7,29 @@ class PredictComponent extends Component {
         super(props);
         this.state = {
             value: '',
-        }
+            predictionDisabled: false,
+            attempts: 0
+        };
+
+        mainStore.on(mainStore.eventTypes.PRICE_FETCHED, this.setPredictionState.bind(this));
     }
     setPredictionValue(value){
         this.setState({value: value}, () => console.log(this.state.value));
+        this.setState({predictionDisabled: true});
+        this.setState((prevState, props) => ({
+            attempts: prevState.attempts + 1
+        }));
+    }
+    setPredictionState(){
+        this.setState({predictionDisabled: false});
     }
     render() {
         return (
             <div>
-                <button onClick={this.setPredictionValue.bind(this, 'negative')}>Negative</button>
-                <button onClick={this.setPredictionValue.bind(this, 'neutral')}>Neutral</button>
-                <button onClick={this.setPredictionValue.bind(this, 'positive')}>Positive</button>
+                <button onClick={this.setPredictionValue.bind(this, 'negative')} disabled={this.state.predictionDisabled}> Negative </button>
+                <button onClick={this.setPredictionValue.bind(this, 'neutral')} disabled={this.state.predictionDisabled}> Neutral </button>
+                <button onClick={this.setPredictionValue.bind(this, 'positive')} disabled={this.state.predictionDisabled}> Positive </button>
+                <p>Attempts: {this.state.attempts} </p>
             </div>)
     }
 }
